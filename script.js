0bd9b85f96b8555ae678b72b23fd12f3037fb3da
Jishnu J <100787432+jishnuj-2001@users.noreply.github.com>
2023-10-09 18:55:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
     toggleBtn.addEventListener('click', function () {
         // Get the computed style
         var computedStyle = window.getComputedStyle(collapseItem);
+        var computedToggleBtn = window.getComputedStyle(toggleBtn);
 
         // Check if the computed display value is 'none'
         if (computedStyle.display === 'none') {
             collapseItem.classList.add('show');
         } else {
             collapseItem.classList.remove('show');
+        }
+        if(computedStyle.display==='flex'){
+            toggleBtn.style.border='3px solid hsl(0, 0%, 70%)';
+        }else{
+            toggleBtn.style.border='2px solid hsl(0, 0%, 80%)';
         }
     });
 });
